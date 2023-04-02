2cdcea80b84f91af99057405ae49a2e23f009c0a
DuendeInexistente <duendeinexistente@gmail.com>
2023-04-02 17:08:49 -0300

    Update

--- dl.sh
+++ dl.sh
@@ -4,7 +4,7 @@ date=$(date)
 echo " " >>"logs/$date.txt"
 echo "URL: $@" >>"logs/$date.txt"
 echo "====================" >>"logs/$date.txt"
-gallery-dl --cookies ./twitter.com_cookies.txt  --download-archive tera.sqlite   --write-metadata --write-info-json --write-tags   --mtime-from-date --filter "extension not in ('psd','zip','clip')"  $@ >> "logs/$date.txt"
+gallery-dl --cookies ./twitter.com_cookies.txt  --download-archive tera.sqlite   --write-metadata --write-info-json --write-tags   --mtime-from-date --filter "extension not in ('psd','clip')"  $@  | tee "logs/$date.txt"
 
 #You may think it's tiny files, but a single run of my archiver generates 6 megs of files. gzipping reduces it to 1.
 #nvim opens the gz files fine.
--- process.sh
+++ process.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+setopt NULL_GLOB
 #for file in gallery-dl/(*booru|*rule34*|e621)/*(|/*).(jpg|png|mp4|gif|wmv|webm|avi) ; do
 #	echo "$file">>test
 #	cat "$file.json" | jq ".source" >>test
@@ -6,8 +7,20 @@
 #	cat "$file.json" | jq ".sourcelist">>test
 #	cat "$file.json" | jq ".source_url">>test
 #done
+gzip logs/*.txt
+
 find gallery-dl -iname "info.json" -delete
 find gallery-dl -empty -delete
+
+
+#cd gallery-dl
+#for file in * ; do
+#	cd $file
+#	find . -mindepth 1 -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "^**\.(avi|gif|jpg|m4v|mp4|png|swf|webm|wmv|zip)$"' ';' -print
+#done
+#cd ..
+
+
 
 if [ -d "./gallery-dl/kemonoparty" ]
 then
@@ -15,7 +28,7 @@ then
 for file in gallery-dl/kemonoparty/*/* ; do
 echo "$file"
 cd "$file"
-	for file in *.(avi|gif|jpg|m4v|mp4|png|swf|webm|wmv|zip) ; do
+	for file in *.(avi|gif|jpg|m4v|mp4|png|swf|webm|wmv|zip|rar|7z) ; do
 		echo "$file"
 
 		cat "$file.json" | jq '.user' | sed 's/^/user: /g' >> "$file.txt"
@@ -31,6 +44,17 @@ cd "$file"
 		sed 's/,$//g' "$file.txt" > /tmp/b
 		cp /tmp/b "$file.txt"
 	done
+	
+	for file in *.(zip|rar|7z); do 
+	unar -f "$file" -o "$file ext"
+	cp "$file.txt" "$file ext/tags.txt"
+	cd "$file ext"
+	echo "zipfile" >> tags.txt
+		for file in ./**/*; do
+			cp tags.txt "$file.txt"
+	done
+	cd ..
+done
 
 cd ../../../..
 done
@@ -195,9 +219,50 @@ done
 
 
 
+if [ -d "./gallery-dl/directlink" ]
+then
+cd gallery-dl/directlink
+for file in *.(avi|gif|jpg|m4v|mp4|png|swf|webm|wmv|zip) ; do
+		echo "$file"
+		echo "source: directlink" >>"$file.txt"
+		cat "$file.json" | jq '.domain' | sed 's/^/domain: /g' >> "$file.txt"
+
+		cat "$file.txt" | sed 's/  / /g' | sed 's/  / /g' | sed 's/  / /g'>/tmp/c
+		cp /tmp/c "$file.txt"
+		sed 's/'\"'//g' "$file.txt" > /tmp/a
+		cp /tmp/a "$file.txt"
+		sed 's/,$//g' "$file.txt" > /tmp/b
+		cp /tmp/b "$file.txt"
+
+done
+cd ../..
+fi
 
 
+if [ -d "./gallery-dl/deviantart" ]
+then
+cd gallery-dl/deviantart
+for file in ./**/*.(avi|gif|jpg|m4v|mp4|png|swf|webm|wmv|zip) ; do
+
+echo $file
+
+cat "$file.json" | jq .folders | sed '$d' | tail -n +2 | sed 's/^/dafolder:/g' >> $file.txt
+cat "$file.json" | jq .author | grep username | sed 's/username/artist/g' >> $file.txt
+cat "$file.json" | jq .is_mature | sed 's/is_mature//g' | sed 's/^/damature:/g' >> $file.txt
+cat "$file.json" | jq .da_category | sed 's/^/dacategory:/g'  >> $file.txt
+
+                cat "$file.txt" | sed 's/  / /g' | sed 's/  / /g' | sed 's/  / /g'>/tmp/c
+                cp /tmp/c "$file.txt"
+                sed 's/'\"'//g' "$file.txt" > /tmp/a
+                cp /tmp/a "$file.txt"
+                sed 's/,$//g' "$file.txt" > /tmp/b
+                cp /tmp/b "$file.txt"
 
 
+done
+
+cd ../..
+
+fi
 
 
